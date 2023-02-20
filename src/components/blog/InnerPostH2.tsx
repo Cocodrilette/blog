248b323f3cc72e912964ee92cdf6b6469b4fba9b
cocodrilette <cocodrilette@gmail.com>
2023-02-19 19:01:53 -0500

    post updated | h2 title style updated

--- src/components/blog/InnerPostH2.tsx
+++ src/components/blog/InnerPostH2.tsx
@@ -1,5 +1,5 @@
 import { ComponentWithContent } from "../../types/components/index";
 
 export default function InnerPostH2({ content }: ComponentWithContent) {
-  return <h2 className="font-bold text-2xl mt-5 pl-3">{content}</h2>;
+  return <h2 className="font-bold text-2xl mt-6 mb-2">{content}</h2>;
 }
