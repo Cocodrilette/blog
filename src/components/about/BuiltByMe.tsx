c3a1f8721c0b53677868f9c266f25ae0b061355f
cocodrilette <cocodrilette@gmail.com>
2023-06-14 19:12:28 -0500

    feat: package manager updated to pnpm chore: projects cards updated

--- src/components/about/BuiltByMe.tsx
+++ src/components/about/BuiltByMe.tsx
@@ -1,73 +1,52 @@
-import ProjectCard from "../commons/ProjectCard";
+import ProjectCard from '../commons/ProjectCard';
 
 export default function BuiltByMe() {
   return (
-    <div className="mb-20">
-      <h2 className="md:text-5xl text-3xl drop-shadow-xl mb-10 font-extrabold text-slate-100">
+    <div className='mb-20'>
+      <h2 className='md:text-5xl text-3xl drop-shadow-xl mb-10 font-extrabold text-slate-100'>
         What I built?
       </h2>
-      <div className="grid md:grid-cols-2 grid-cols-1 gap-10 ">
+      <div className='grid md:grid-cols-2 grid-cols-1 gap-10 '>
         <ProjectCard
-          imageUrl="/images/dotlabs.png"
-          title="dotlabs(Medellin)"
-          description="Landing page built to dotlabs() hackathon built with Astro and Tailwind."
-          link="https://dotlabs.academy/"
+          imageUrl='/images/dotlabs.png'
+          title='dotlabs()'
+          description='dotlabs() website. A community where Web3 is taught to anyone interested in writing the future of the internet. Site built with Next.js.'
+          link='https://dotlabs.academy/'
         />
 
         <ProjectCard
-          imageUrl="/images/dotlabs-calendar.png"
-          title="dotlabs() Workshops calendar"
-          description="Calendar built for the dotlabs() workshops using Next.js and Tailwind."
-          link="https://dotlabs-workshops-calendar.vercel.app/"
+          imageUrl='/images/alianza-juridica.png'
+          title='Alianza Juridica S.A.S'
+          description='Web page built for a law firm in Medellin, Colombia. Built with Next.js and Tailwind.'
+          link='https://alianza-juridica.com/'
         />
 
         <ProjectCard
-          imageUrl="/images/dotlabs-registration.png"
-          title="dotlabs() Registration system"
-          description="A Solidity Smart Contract + React UI to register your interest in dotlabs() events."
-          link="https://dotlabs-workshops-calendar.vercel.app/"
+          imageUrl='/images/qrCodeGen.png'
+          title='QR code generator'
+          description='A simple way to generate a beautiful QR.'
+          link='https://licencia-de-perreo.vercel.app/'
         />
 
         <ProjectCard
-          imageUrl="/images/alianza-juridica.png"
-          title="Alianza Juridica S.A.S"
-          description="Web page built for a law firm in Medellin, Colombia. Built with Next.js and Tailwind."
-          link="https://alianza-juridica.com/"
+          imageUrl='/images/gotify.png'
+          title='Gotify'
+          description='MVP built for the Web3ForU hackathon using React (Next.js) and Tailwind.'
+          link='https://github.com/Cocodrilette/hackathon-web3forU'
         />
 
         <ProjectCard
-          imageUrl="/images/qrCodeGen.png"
-          title="QR code generator"
-          description="A simple way to generate a beautiful QR."
-          link="https://licencia-de-perreo.vercel.app/"
+          imageUrl='/images/hold-shift.gif'
+          title='Hold Shift to Check Checkboxes'
+          description='Holds shift to check multiple checkboxes feature using vanilla JS.'
+          link='https://github.com/Cocodrilette/30-days-of-javascript/tree/main/hold-shift-to-check-multiple-checkboxes'
         />
 
         <ProjectCard
-          imageUrl="/images/gotify.png"
-          title="Gotify"
-          description="MVP built for the Web3ForU hackathon using React (Next.js) and Tailwind."
-          link="https://github.com/Cocodrilette/hackathon-web3forU"
-        />
-
-        <ProjectCard
-          imageUrl="/images/hold-shift.gif"
-          title="Hold Shift to Check Checkboxes"
-          description="Holds shift to check multiple checkboxes feature using vanilla JS."
-          link="https://github.com/Cocodrilette/30-days-of-javascript/tree/main/hold-shift-to-check-multiple-checkboxes"
-        />
-
-        <ProjectCard
-          imageUrl="/images/ecdsa.png"
-          title="ECDSA Node"
-          description="A React frontend that allow you to send signed messages using Elliptic Curve Digital Sign Algorithm from ethereum cryptography package."
-          link="https://github.com/Cocodrilette/ECDSA-Node"
-        />
-
-        <ProjectCard
-          imageUrl="/images/raccoon.jpg"
-          title="This site"
+          imageUrl='/images/raccoon.jpg'
+          title='This site'
           description="Obviously this site also was built be me... and yes, that's a raccoon. I'm using Next.js, Tailwind and TypeScript."
-          link="https://github.com/Cocodrilette/blog"
+          link='https://github.com/Cocodrilette/blog'
         />
       </div>
     </div>
--- src/components/commons/ProjectCard.tsx
+++ src/components/commons/ProjectCard.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import { ProjectCardProps } from "../../types/components/index";
+import Image from 'next/image';
+import { ProjectCardProps } from '../../types/components/index';
 
 export default function ProjectCard({
   imageUrl,
@@ -8,25 +8,25 @@ export default function ProjectCard({
   link,
 }: ProjectCardProps) {
   return (
-    <div className="bg-gray-900 rounded-md border-2 border-beige shadow-md shadow-black xl:blur-sm hover:blur-none transition duration-200">
+    <div className='bg-gray-900 rounded-md border-2 border-beige shadow-md shadow-black xl:blur-sm hover:blur-none transition duration-200'>
       <a
-        className="flex flex-col"
+        className='flex flex-col'
         href={link}
-        aria-details="Link to the project showed"
-        target="_blank"
-        rel="noreferrer"
+        aria-details='Link to the project showed'
+        target='_blank'
+        rel='noreferrer'
       >
         <div>
           <Image
-            className="rounded-t-md max-h-400"
+            className='rounded-t-md max-h-400'
             src={imageUrl}
             alt={title}
             width={1000}
             height={700}
           />
-          <div className="flex flex-col gap-2 p-3">
-            <h3 className="md:text-2xl text font-bold">{title}</h3>
-            <p className="text-sm text-slate-400">{description}</p>
+          <div className='flex flex-col gap-2 p-3'>
+            <h3 className='md:text-2xl text font-bold'>{title}</h3>
+            <p className='text-sm text-slate-400'>{description}</p>
           </div>
         </div>
       </a>
