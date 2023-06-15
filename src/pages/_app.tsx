87bf4b42ab10cd07f5a24c71c30918ab55a3a0e9
cocodrilette <cocodrilette@gmail.com>
2023-06-14 20:08:15 -0500

    feat: vercel analytics added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
-import "@app/styles/globals.css";
-import { Quicksand } from "@next/font/google";
-import type { AppProps } from "next/app";
+import '@app/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
