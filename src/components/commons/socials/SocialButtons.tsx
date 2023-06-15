80366385ecb54c9b48b62a6cdd3a00953840e472
cocodrilette <cocodrilette@gmail.com>
2023-06-14 19:22:20 -0500

    feat: buttons refactored and gitlab button added

--- src/components/commons/socials/SocialButtons.tsx
+++ src/components/commons/socials/SocialButtons.tsx
@@ -1,32 +1,45 @@
-import { BsTwitter, BsGithub, BsLinkedin } from "react-icons/bs";
+import { BsTwitter, BsGithub, BsLinkedin } from 'react-icons/bs';
+import { RiGitlabFill } from 'react-icons/ri';
 
 export default function SocialsButtons() {
+  const sharedComponentClasses =
+    'flex gap-2 items-center w-[200px] transition-colors p-2 justify-between rounded-md border-2 shadow-md shadow-black';
+  const sharedButtonClasses = 'text-slate-100 text-xl';
+
   return (
-    <div className="flex flex-col gap-5 mt-10">
+    <div className='flex flex-col gap-5 mt-10'>
       <a
-        className="flex gap-2 items-center w-[200px] bg-[#1da1f2] p-2 justify-between rounded-md border-2 shadow-md shadow-black"
-        href="https://twitter.com/cocodrilette"
-        target="_blank"
-        rel="noreferrer"
+        className={sharedComponentClasses + ' bg-[#1da1f2] hover:bg-[#068ce0]'}
+        href='https://twitter.com/cocodrilette'
+        target='_blank'
+        rel='noreferrer'
       >
-        Twitter <BsTwitter className="text-slate-100 text-xl" />
+        Twitter <BsTwitter className={sharedButtonClasses} />
       </a>
       <a
-        className="flex gap-2 items-center w-[200px] bg-[#161b22] p-2 justify-between rounded-md border-2 shadow-md shadow-black"
-        href="https://github.com/Cocodrilette"
-        target="_blank"
-        rel="noreferrer"
+        className={sharedComponentClasses + ' bg-[#161b22] hover:bg-[#0b0d11]'}
+        href='https://github.com/Cocodrilette'
+        target='_blank'
+        rel='noreferrer'
       >
-        Github <BsGithub className="text-slate-100 text-xl" />
+        Github <BsGithub className={sharedButtonClasses} />
       </a>
       <a
-        className="flex gap-2 items-center w-[200px] bg-[#0a66c2] p-2 justify-between rounded-md border-2 shadow-md shadow-black"
-        href="https://www.linkedin.com/in/juan-fernando-cogollo-doria/"
-        target="_blank"
-        rel="noreferrer"
+        className={sharedComponentClasses + ' bg-[#e94d27] hover:bg-[#d83c15]'}
+        href='https://gitlab.com/Cocodrilette'
+        target='_blank'
+        rel='noreferrer'
+      >
+        GitLab <RiGitlabFill className={sharedButtonClasses} />
+      </a>
+      <a
+        className={sharedComponentClasses + ' bg-[#0a66c2] hover:bg-[#005cb7]'}
+        href='https://www.linkedin.com/in/juan-fernando-cogollo-doria/'
+        target='_blank'
+        rel='noreferrer'
       >
         LinkedIn
-        <BsLinkedin className="text-slate-100 text-xl" />
+        <BsLinkedin className={sharedButtonClasses} />
       </a>
     </div>
   );
