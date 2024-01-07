e4e1d77d9e6f0987714f3488375d73dd06853569
cocodrilette <cocodrilette@gmail.com>
2024-01-07 18:10:30 -0500

    Fix: Margin fixed

--- src/components/about/HowIAm.tsx
+++ src/components/about/HowIAm.tsx
@@ -6,7 +6,7 @@ const fira_code = Fira_Code({
 
 export function HowIAm() {
   return (
-    <div className={`${fira_code.className} my-10`}>
+    <div className={`${fira_code.className} mb-10`}>
       <h1 className={`text-3xl md:text-4xl`}>
         <span className="text-dimmedPink">/</span>cocodrilette
       </h1>
